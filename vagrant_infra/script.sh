d8abfbe1ad2763840f70e7d56c439137e7acb288
gustavo.a.jesus <gustavo.a.jesus@ericssoninovacao.com.br>
2021-09-14 17:23:25 -0300

    atualização

--- vagrant_infra/script.sh
+++ vagrant_infra/script.sh
@@ -1,37 +1,24 @@
-    # Garantindo as chaves
-    KEY_PATH='/vagrant/files'
-    mkdir -p /root/.ssh
-    cp $KEY_PATH/key /root/.ssh/id_rsa
-    cp $KEY_PATH/key.pub /root/.ssh/id_rsa.pub
-    cp $KEY_PATH/key.pub /root/.ssh/authorized_keys
-    chmod 400 /root/.ssh/id_rsa*
-    cat /root/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+#!/usr/bin/env bash
 
-    HOST_IP="200.100.50"
+# Garantindo as chaves
+KEY_PATH="/vagrant/files"
+mkdir -p /root/.ssh
+cp $KEY_PATH/id_rsa /root/.ssh/id_rsa
+cp $KEY_PATH/id_rsa.pub /root/.ssh/id_rsa.pub
+cp $KEY_PATH/id_rsa.pub /root/.ssh/authorized_keys
+chmod 400 /root/.ssh/id_rsa*
+cat /root/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 
+#HOST_IP="192.168.56.3"
 
-    # Garantindo os hosts
-    HOSTS=$(head -n7 /etc/hosts)
-    echo -e "$HOSTS" > /etc/hosts
-    echo '200.100.50.10 worker01.4labs.example' >> /etc/hosts
-    echo '200.100.50.20 worker02.4labs.example' >> /etc/hosts
-    echo '200.100.50.30 manager01.4labs.example' >> /etc/hosts
-    echo '200.100.50.40 monitoring.4labs.example' >> /etc/hosts
-    echo '200.100.50.50 registry.4labs.example' >> /etc/hosts
+# Garantindo os hosts
+echo '192.168.56.10 node1.lab' >> /etc/hosts
+echo '192.168.56.20 node2.lab' >> /etc/hosts
+echo '192.168.56.30 node3.lab' >> /etc/hosts
 
-    # Garantindo q o usuario suporte esteja presente
-    sudo adduser suporte
-    #usermod -aG wheel suporte
-
-    # Criando arquivo de SWAP 
-    fallocate -l 1G /swapfile
-    chmod 600 /swapfile
-    mkswap /swapfile
-    echo -e "/swapfile swap swap defaults 0 0" >> /etc/fstab
-    swapon -a
-
-    # for Centos
-    # sudo -i
-    # adduser suporte
-    # usermod -aG wheel suporte
-    # passwd suporte
+# Criando arquivo de SWAP 
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+echo -e "/swapfile swap swap defaults 0 0" >> /etc/fstab
+swapon -a
